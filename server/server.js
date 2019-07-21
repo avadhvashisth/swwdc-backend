d930d157ce7b10b5f399e1a5013f484c61871e66
avadhvashisth <avadh.vashisth@gmail.com>
2019-07-21 11:27:44 +0530

    minor changes

--- server/server.js
+++ server/server.js
@@ -49,8 +49,8 @@ var app = express();
 //cors
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization");
   next();
 });
 
